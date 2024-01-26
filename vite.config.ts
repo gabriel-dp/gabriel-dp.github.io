02150f3e57c47c3194cb915f55fced8099bc2333
gabriel-dp <gabriel.meira.2004@gmail.com>
2024-01-26 17:20:25 -0300

    :rocket: Fix vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./",
+	base: "https://gabriel-dp.github.io/",
 	plugins: [react(), tsconfigPaths(), svgr()],
 });
+
