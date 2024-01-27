fae6437809c69389ad92c101186f496ed8bdb8e2
gabriel-dp <gabriel.meira.2004@gmail.com>
2024-01-27 15:03:00 -0300

    :rocket: Add special support to vercel deployment

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,20 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 
 import { Page, Home, Projects, NotFound, Resume } from "@/pages";
 
 export default function Router() {
+	// Alternates router if it is a Vercel deployment
+	const BaseRouter = import.meta.env.VITE_VERCEL_ENV ? BrowserRouter : HashRouter;
+
 	return (
-		<HashRouter>
+		<BaseRouter>
 			<Routes>
 				<Route path={Page.Home} element={<Home />} />
 				<Route path={Page.Projects} element={<Projects />} />
 				<Route path={Page.Resume} element={<Resume />} />
 				<Route path={Page.NotFound} element={<NotFound />} />
 			</Routes>
-		</HashRouter>
+		</BaseRouter>
 	);
 }
 
