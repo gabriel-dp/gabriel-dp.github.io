85c3e131b37595ca894d522a851873c394136dc4
gabriel-dp <gabriel.meira.2004@gmail.com>
2024-02-05 18:51:04 -0300

    :bug: Fix alternating router bug

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,8 @@ import { Page, Home, About, Projects, Resume, NotFound } from "@/pages";
 
 export default function Router() {
 	// Alternates router if it is a Vercel deployment
-	const BaseRouter = import.meta.env.VITE_VERCEL_ENV == "production" ? BrowserRouter : HashRouter;
+	// .env must be empty when building to deploy gh-pages
+	const BaseRouter = import.meta.env.VITE_VERCEL_ENV ? BrowserRouter : HashRouter;
 
 	return (
 		<BaseRouter>
