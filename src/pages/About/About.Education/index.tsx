dec718100a1a472e5e4f59b314f87ebfed3a6a72
gabriel-dp <gabriel.meira.2004@gmail.com>
2024-02-06 14:23:33 -0300

    :sparkles: Initial education timeline

--- src/pages/About/About.Education/index.tsx
+++ src/pages/About/About.Education/index.tsx
@@ -1,9 +1,24 @@
+import about from "@/configs/about.json";
 import SectionWrapper from "@/components/layout/SectionWrapper";
+
+import { EducationContainer, EducationPath, SingleEducation, VerticalLine } from "./styles";
 
 export default function AboutEducation() {
 	return (
 		<SectionWrapper $id="education" title="Education">
-			<p>* in progress *</p>
+			<EducationContainer>
+				<VerticalLine />
+				<EducationPath>
+					{about.educations.map((education) => (
+						<SingleEducation key={education.name}>
+							<p>{education.name}</p>
+							<p>
+								{education.start} - {education.end}
+							</p>
+						</SingleEducation>
+					))}
+				</EducationPath>
+			</EducationContainer>
 		</SectionWrapper>
 	);
 }
