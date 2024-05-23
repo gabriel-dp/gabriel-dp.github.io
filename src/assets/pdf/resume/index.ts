a779f1c0df867c8a2cdd6e0f51fe4a06233178b2
gabriel-dp <gabriel.meira.2004@gmail.com>
2024-05-22 21:16:38 -0300

    :bug: Change resume's dynamic import to pseudo-dynamic

---
+++ src/assets/pdf/resume/index.ts
@@ -0,0 +1,9 @@
+import resumeEnUs from "./resume-en-US.pdf";
+import resumePtBr from "./resume-pt-BR.pdf";
+
+const resumes = {
+	"resume-en-US": resumeEnUs,
+	"resume-pt-BR": resumePtBr,
+};
+
+export default resumes;
--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { FaFilePdf } from "react-icons/fa";
 
 import PageWrapper from "@/components/layout/PageWrapper";
@@ -6,33 +5,24 @@ import { LinkComponentExternal } from "@/components/utilities/LinkComponent";
 import Navbar from "@/components/layout/Navbar";
 import SectionWrapper from "@/components/layout/SectionWrapper";
 import Button from "@/components/layout/Button";
+import resumes from "@/assets/pdf/resume";
 import useLanguage, { useLanguageConfigs } from "@/contexts/language/useLanguage";
 
 import { ObjectPDF } from "./styles";
 
 export default function Resume() {
-	const [resume, setResume] = useState("");
-
 	const t = useLanguage();
 	const language = useLanguageConfigs().language;
 
-	// Updates resume directory when language changes
-	useEffect(() => {
-		// Dynamic imports should be relative and do not have alias
-		const RESUME_FOLDER = "../../assets/pdf";
-		const RESUME_FILE = `resume-${language}.pdf`;
-		const path = `${RESUME_FOLDER}/${RESUME_FILE}`;
-
-		import(/* @vite-ignore */ path).then((file) => setResume(file.default));
-	}, [language]);
+	const file = resumes[`resume-${language}` as keyof typeof resumes];
 
 	return (
 		<PageWrapper>
 			<Navbar />
 			<SectionWrapper $id="resume" title={t`sections.resume.title`} sub={language}>
-				<ObjectPDF data={resume}>
+				<ObjectPDF data={file}>
 					<Button>
-						<LinkComponentExternal to={resume} label="Resume">
+						<LinkComponentExternal to={file} label="Resume">
 							{t`resume.external-button`} <FaFilePdf className="icon" />
 						</LinkComponentExternal>
 					</Button>
